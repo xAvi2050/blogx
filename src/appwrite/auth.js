7724f6eb04ae2a0c3cd247138a9bd1408fe8467d
xAvi2050 <cheetahmindset7@gmail.com>
2025-11-19 10:34:43 +0530

    feat: implement Appwrite AuthService (signup, login, getCurrentUser, logout)

---
+++ src/appwrite/auth.js
@@ -0,0 +1,46 @@
+import config from '../config.js';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appWriteUrl)
+            .setProject(config.appWriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        const user = await this.account.create(
+            ID.unique(),
+            email,
+            password,
+            name
+        );
+
+        try {
+            return await this.login({ email, password });
+        } catch {
+            return user; // Account created but cannot login yet
+        }
+    }
+
+    async login({ email, password }) {
+        return await this.account.createEmailPasswordSession(email, password);
+    }
+
+    async getCurrentUser() {
+        return await this.account.get();
+    }
+
+    async logout() {
+        return await this.account.deleteSessions('current');
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
